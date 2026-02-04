aeb50f0d206a95fbde74553de39766f15e447b96
Daniela R <daniramirezg@udistrital.edu.co>
2026-02-03 20:44:59 -0500

    con mapa

---
+++ programa.js
@@ -0,0 +1,7 @@
+var map = L.map('map').setView([4.609722, -74.08175], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([4.609722, -74.08175]).addTo(map);
